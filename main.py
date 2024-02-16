1ac594e6ee5dd352621ca0b4210cb26d88001c48
hidriali <159208504+hidriali@users.noreply.github.com>
2024-02-16 06:41:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #display "I'm learning how to proramm in python"
-print("I'm learning how to program in python")  
+print("I'm learning how to program in python.")  
 # display "that's awesome"
-print("That's awesome")
+print("That's awesome!")
